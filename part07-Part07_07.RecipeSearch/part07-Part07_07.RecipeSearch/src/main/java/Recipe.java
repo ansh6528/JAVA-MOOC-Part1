8e8cf159d2e8d459bf529e778057f2a17651a046
ansh6528 <88899923+ansh6528@users.noreply.github.com>
2022-11-30 15:33:05 +0530

    Add files via upload

---
+++ part07-Part07_07.RecipeSearch/part07-Part07_07.RecipeSearch/src/main/java/Recipe.java
@@ -0,0 +1,57 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ansh.singh
+ */
+public class Recipe {
+    private String name;
+    private int time;
+    private ArrayList<String> ingredients;
+    
+    public Recipe(String name, int time, ArrayList<String> ingredients) {
+        this.name = name;
+        this.time = time;
+        this.ingredients = ingredients;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setTime(int time) {
+        this.time = time;
+    }
+    
+    public void setIngredients(ArrayList<String> ingredients) {
+        this.ingredients = ingredients;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getTime() {
+        return this.time;
+    }
+    
+    public ArrayList<String> getIngredient() {
+        return this.ingredients;
+    }
+    
+    public boolean hasIngredient(String ingredient) {
+        for (String x : this.ingredients) {
+            if (x.equals(ingredient)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ part07-Part07_07.RecipeSearch/part07-Part07_07.RecipeSearch/src/main/java/UserInterface.java
@@ -0,0 +1,116 @@
+
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ansh.singh
+ */
+public class UserInterface {
+    private Scanner scanner;
+    private ArrayList<Recipe> recipeList = new ArrayList<>();
+    
+    public UserInterface(Scanner scanner) {
+        this.scanner = scanner;
+    }
+    
+    public void start() {
+        System.out.println("File to read: ");
+        String fileName = scanner.nextLine();
+        readFile(fileName);
+        
+        System.out.println("");
+        
+        System.out.println("Commands: \nlist - lists the recipes \nstop - stops the program"
+                + "\nfind name - searches recipes by name \nfind cooking time - searches recipes by cooking time"
+                + " \nfind ingredient - searches recipes by ingredient");
+        System.out.println("");
+        
+        while (true) {
+            System.out.println("Enter command: ");
+            String command = scanner.nextLine();
+            
+            if (command.equalsIgnoreCase("stop")) {
+                break;
+            }
+            
+            if (command.equalsIgnoreCase("list")) {
+                for(Recipe r : recipeList) {
+                    System.out.println(r);
+                }
+            }
+            
+            if (command.equalsIgnoreCase("find name")) {
+                System.out.println("Searched word: ");
+                String findWord = scanner.nextLine();
+                
+                for (Recipe r : recipeList) {
+                    if (r.getName().contains(findWord)) {
+                        System.out.println(r);
+                    }
+                }
+            }
+            
+            if (command.equalsIgnoreCase("find cooking time")) {
+                System.out.println("Max cooking time: ");
+                int maxTime = scanner.nextInt();
+                
+                for (Recipe r : recipeList) {
+                    if (r.getTime() <= maxTime) {
+                        System.out.println(r);
+                    }
+                }
+            }
+            
+            if (command.equalsIgnoreCase("find ingredient")) {
+                System.out.println("Ingredient: ");
+                String findIngredient = scanner.nextLine();
+                
+                for (Recipe r : recipeList) {
+                    if (r.hasIngredient(findIngredient)) {
+                        System.out.println(r);
+                    }
+                }
+            }
+        }
+    }
+    
+    public void readFile(String fileName) {
+        try (Scanner fileReader = new Scanner(Paths.get("recipes.txt"))) {
+            while(fileReader.hasNextLine()) {
+                String line = fileReader.nextLine();
+                
+                if (line.isEmpty()) {
+                    continue;
+                }
+                
+                String name = line;
+                
+                int time = Integer.valueOf(fileReader.nextLine());
+                ArrayList <String> ingredientsList = new ArrayList();
+                
+                while (fileReader.hasNextLine()) {
+                    String ingredient = fileReader.nextLine();
+                    if (ingredient.isEmpty()) {
+                        break;
+                    }
+                    ingredientsList.add(ingredient);
+                }
+                recipeList.add(new Recipe(name, time, ingredientsList));
+            }
+        } catch (Exception e) {
+            System.out.println("Error" + e.getMessage());
+        }
+       
+            
+        
+    }
+
+}
